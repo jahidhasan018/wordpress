8aebab5a3a1f4120ff4eba0a3ad1f4ce65cf560c
Jahid Hasan <jahid0676@gmail.com>
2018-11-13 19:12:41 +0600

    Update wp-filters-hook.php

--- wp-filters-hook.php
+++ wp-filters-hook.php
@@ -1,5 +1,18 @@
 /*======================
-// Wordpress Protected Content Filter 
+// Add class to menu li // filter -- nav_menu_css_class
+========================*/
+
+function alpha_menu_li_class( $classes, $item ){
+	$classes[] = 'list-inline-item';
+	return$classes; 
+}
+add_filter( 'nav_menu_css_class', 'alpha_menu_li_class', 10, 2 );
+
+
+
+
+/*======================
+// Wordpress Protected Content Filter // filter -- the_excerpt
 ========================*/
 function alpha_pasword_protected_content($excerpt){
 	if ( ! post_password_required() ) {
@@ -10,7 +23,7 @@ function alpha_pasword_protected_content($excerpt){
 }
 add_filter( 'the_excerpt', 'alpha_pasword_protected_content' );
 
-// Remove Protected content title
+// Remove Protected content title // filter -- protected_title_format
 function alpha_protected_title_change(){
 	return "%s";
 }
