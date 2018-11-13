2c4ce0eb28b757b4b41366637bbd4b608f3e35f2
Jahid Hasan <jahid0676@gmail.com>
2018-11-13 19:11:15 +0600

    Update wp-filters-hook.php

--- wp-filters-hook.php
+++ wp-filters-hook.php
@@ -9,3 +9,9 @@ function alpha_pasword_protected_content($excerpt){
 	}
 }
 add_filter( 'the_excerpt', 'alpha_pasword_protected_content' );
+
+// Remove Protected content title
+function alpha_protected_title_change(){
+	return "%s";
+}
+add_filter( 'protected_title_format', 'alpha_protected_title_change' );
