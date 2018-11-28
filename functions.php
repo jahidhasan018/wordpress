d5ecfde90bfb38507333cf76d9e2e51e3e8975bc
Jahid Hasan <jahid0676@gmail.com>
2018-11-28 12:49:39 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,30 @@
 <?php
+// **************************//
+// Custom Wordpress excerpt length
+function excerpt($limit) {
+  $excerpt = explode(' ', get_the_excerpt(), $limit);
+  if (count($excerpt)>=$limit) {
+    array_pop($excerpt);
+    $excerpt = implode(" ",$excerpt).'...';
+  } else {
+    $excerpt = implode(" ",$excerpt);
+  }	
+  $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+  return $excerpt;
+}
 
-echo "not added yet";
+// **************************//
+// Wordpress Custom Pagination
+function philosophy_pagination() {
+    global $wp_query;
+    $links = paginate_links( array(
+        'current'  => max( 1, get_query_var( 'paged' ) ),
+        'total'    => $wp_query->max_num_pages,
+        'type'     => 'list',
+        'mid_size' => 3
+    ) );
+    $links = str_replace( "page-numbers", "pgn__num", $links );
+    $links = str_replace( "<ul class='pgn__num'>", "<ul>", $links );
+    $links = str_replace( "next pgn__num", "pgn__next", $links );
+    $links = str_replace( "prev pgn__num", "pgn__prev", $links );
+    
